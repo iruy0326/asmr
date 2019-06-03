3e8bcb22bdb2a9c502207b1cf95d7fb1c7caf9f3
iruy0326 <eulee98@gmail.com>
2019-06-03 15:22:27 +0900

    image change

--- app/src/main/java/com/example/asmr/Main2Activity_tapping.java
+++ app/src/main/java/com/example/asmr/Main2Activity_tapping.java
@@ -14,32 +14,39 @@ import java.io.IOException;
 public class Main2Activity_tapping extends AppCompatActivity {
     MediaPlayer mp;
     SeekBar seekBar;
-    Button btn1, btn2, btn3;
-    TextView text;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    TextView text1, text2, text3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2_tapping);
 
-        btn1 = (Button)findViewById(R.id.button1);
-        btn2 = (Button)findViewById(R.id.button2);
-        btn3 = (Button)findViewById(R.id.button3);
-        text = (TextView)findViewById(R.id.text1);
+        btn1 = (Button) findViewById(R.id.button1_1);
+        btn2 = (Button) findViewById(R.id.button1_2);
+        btn3 = (Button) findViewById(R.id.button1_3);
+        text1 = (TextView) findViewById(R.id.text1);
         mp = MediaPlayer.create(Main2Activity_tapping.this, R.raw.chacha);
 
-        seekBar = (SeekBar)findViewById(R.id.playbar);
+        seekBar = (SeekBar) findViewById(R.id.playbar1);
         seekBar.setVisibility(ProgressBar.VISIBLE);
         seekBar.setMax(mp.getDuration());
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(fromUser) { mp.seekTo(progress); }
-                int m = progress / 60000; int s = (progress % 60000) / 1000; String strTime = String.format("%02d:%02d", m, s);
-                text.setText(strTime); }
+                if (fromUser) {
+                    mp.seekTo(progress);
+                }
+                int m = progress / 60000;
+                int s = (progress % 60000) / 1000;
+                String strTime = String.format("%02d:%02d", m, s);
+                text1.setText(strTime);
+            }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
@@ -58,33 +65,32 @@ public class Main2Activity_tapping extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.stop();
-                try {
-                    mp.prepare();
-                }
-                catch(IOException ie) {
-                    ie.printStackTrace();
-                }
-                    mp.seekTo(0);
+
+                mp.pause();
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                mp.pause();
+                mp.stop();
+                try {
+                    mp.prepare();
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+                mp.seekTo(0);
             }
         });
-
-
     }
-    public void Thread(){
-        Runnable task = new Runnable(){
 
 
-            public void run(){
+    public void Thread() {
+        Runnable task = new Runnable() {
+
+
+            public void run() {
                 // 음악이 재생중일때
-                while(mp.isPlaying()){
+                while (mp.isPlaying()) {
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -98,6 +104,9 @@ public class Main2Activity_tapping extends AppCompatActivity {
         Thread thread = new Thread(task);
         thread.start();
     }
+}
+
+
 
 
 //    @Override
@@ -121,4 +130,4 @@ public class Main2Activity_tapping extends AppCompatActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-}
+
--- app/src/main/java/com/example/asmr/Main2Activity_whisper.java
+++ app/src/main/java/com/example/asmr/Main2Activity_whisper.java
@@ -1,13 +1,104 @@
 package com.example.asmr;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class Main2Activity_whisper extends AppCompatActivity {
+    MediaPlayer mp;
+    SeekBar seekBar;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    TextView text1, text2, text3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2_whisper);
+        btn1 = (Button) findViewById(R.id.button1_1);
+        btn2 = (Button) findViewById(R.id.button1_2);
+        btn3 = (Button) findViewById(R.id.button1_3);
+        text1 = (TextView) findViewById(R.id.text1);
+        mp = MediaPlayer.create(Main2Activity_whisper.this, R.raw.chacha);
+
+        seekBar = (SeekBar) findViewById(R.id.playbar1);
+        seekBar.setVisibility(ProgressBar.VISIBLE);
+        seekBar.setMax(mp.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mp.seekTo(progress);
+                }
+                int m = progress / 60000;
+                int s = (progress % 60000) / 1000;
+                String strTime = String.format("%02d:%02d", m, s);
+                text1.setText(strTime);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+
+                Thread();
+
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mp.pause();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.stop();
+                try {
+                    mp.prepare();
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+                mp.seekTo(0);
+            }
+        });
+    }
+    public void Thread() {
+        Runnable task = new Runnable() {
+
+
+            public void run() {
+                // 음악이 재생중일때
+                while (mp.isPlaying()) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    seekBar.setProgress(mp.getCurrentPosition());
+                }
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
     }
 }
